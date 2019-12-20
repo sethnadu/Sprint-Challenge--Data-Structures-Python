d0158b7015b212b32994e988b9a66896bbf33b87
Seth Nadu <seth.nadu@gmail.com>
2019-12-20 11:27:51 -0500

    names.py working under a sec with binary_search_tree import

--- names/names.py
+++ names/names.py
@@ -1,24 +1,42 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# current operation is O(n^2)
+# Should be a total of 64 duplicates, current runtime is 12 seconds
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
+# ***************************************************************************
+# 64 duplicates were found, under 1 sec run time
+duplicates = []
+names_bst = BinarySearchTree('')
+
+for name_1 in names_1:
+    names_bst.insert(name_1)
+
+for name_2 in names_2:
+    if names_bst.contains(name_2):
+        duplicates.append(name_2)
+
+# ***************************************************************************
+print(duplicates)
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
